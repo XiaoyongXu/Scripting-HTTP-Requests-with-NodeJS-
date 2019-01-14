468cce00127db91618da37923439e220a0247b68
XiaoyongXu <xxy1994212@gmail.com>
2019-01-14 20:55:48 +0000

    step4 complete

---
+++ step4PrintingWithCallBack.js
@@ -0,0 +1,35 @@
+var https = require('https');
+var body = '';
+
+function getAndPrintHTML (options, callback) {
+
+  /* Add your code here */
+  https.get(options, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+  
+    // the callback is invoked when a `data` chunk is received
+
+    response.on('data', function(chunk) {
+      body += chunk;
+    });
+  
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function(){
+      printHTML (body);
+    });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getAndPrintHTML(requestOptions, printHTML);
